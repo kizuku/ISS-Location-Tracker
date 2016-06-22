3b02df2491e6d5d317a38d50f233110bd7e263dd
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-22 13:06:20 -0400

    Hello

--- Hello.py
+++ Hello.py
@@ -1,12 +1,13 @@
 import os 
 from flask import Flask
-from flask import url_for, redirect
+from flask import url_for, render_template
+import Interface.html
 
 app = Flask(__name__)
 
 @app.route("/")
 def html():
-    return redirect(url_for('static', filename='Interface.html'))
+    return render_template(url_for("static", filename="Interface.html"))
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
