2203c28f1bc613d7e916116be475c12d5be88926
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-22 14:27:27 -0400

    why

--- Hello.py
+++ Hello.py
@@ -1,13 +1,6 @@
-import os 
-from flask import Flask
-from flask import request
 from flask import render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def root():
-    return render_template('interface.html')
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
