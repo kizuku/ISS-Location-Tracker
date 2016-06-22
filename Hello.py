0539c821343de710aae8d496c2cfac3ba288adbb
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-22 13:30:50 -0400

    Revisions

--- Hello.py
+++ Hello.py
@@ -4,8 +4,10 @@ from flask import request
 
 app = Flask(__name__, static_url_path='')
 
-@app.route('/')
-def html():
+@app.route('/Interface/')
+def interface():
     return app.send_static_file('Interface.html')
 
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host = '0.0.0.0', port=port)
