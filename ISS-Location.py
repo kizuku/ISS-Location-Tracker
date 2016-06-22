99a030308f85e09e6a687bc4466d9b342b0bf3ba
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-21 21:34:40 -0400

    Initialization

---
+++ ISS-Location.py
@@ -0,0 +1,10 @@
+import urllib2
+import json
+
+while True:
+    req = urllib2.Request("http://api.open-notify.org/iss-now.json")
+    response = urllib2.urlopen(req)
+    obj = json.loads(response.read())
+    print obj['timestamp']
+    print obj['iss_position']['latitude'], obj['iss_position']['longitude']
+
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python ISS-Location.py
