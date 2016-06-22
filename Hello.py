3c73e4ce951ed8a983f66139b80dc69c921f1842
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-22 14:16:10 -0400

    Hello

--- Hello.py
+++ Hello.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
 @app.route('/')
 def root():
