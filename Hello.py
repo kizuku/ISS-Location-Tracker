f67ad00d61f301390c3a1da7b0d991dea1168cfd
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-22 16:25:16 -0400

    Hello.py

--- Hello.py
+++ Hello.py
@@ -1,9 +1,15 @@
 from flask import render_template
 from flask import Flask, request
 
-@app.route('/')
-
 app = Flask(__name__)
 
+@app.route('/')
 def hello():
+    return "Hello!"
+
+@app.route('/interface')
+def interface():
     return render_template('interface.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
