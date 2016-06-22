d05cfb75e8243741492db81ea64d3c269eccaa33
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-21 22:06:20 -0400

    Edits

--- Hello.py
+++ Hello.py
@@ -1,1 +1,12 @@
-print "Hello"
+import os 
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
