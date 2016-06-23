134b740580e7359bdcbc25e075118b0c9f4b980c
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-23 12:44:21 -0400

    Hello

--- Hello.py
+++ Hello.py
@@ -5,11 +5,12 @@ app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def hello():
-    return "Hello!"
-
-@app.route('/interface')
-def interface():
     return app.send_static_file('welcome.html')
+#    return "Hello!"
+
+#@app.route('/interface')
+#def interface():
+#    return app.send_static_file('welcome.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
