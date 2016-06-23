d32dfabd5d13c23acd028b2d5976448b5efbba56
Zachary Zhao <dukezmz1999@gmail.com>
2016-06-23 12:55:08 -0400

    Hello

--- Hello.py
+++ Hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('welcome.html')
+    return render_template('interface.html')
 #    return "Hello!"
 
 #@app.route('/interface')
